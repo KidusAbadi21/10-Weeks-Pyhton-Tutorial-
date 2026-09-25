2da8700f0f793cde3cef16b93452098a174d9630
KidusAbadi21 <kidusabadi21@gmail.com>
2026-09-25 19:28:56 +0300

    Add a Python file mini-project I made in the video

---
+++ Week 03/Part Three.py
@@ -0,0 +1,16 @@
+num1 = int(input("pls enter a number: "))
+num2 = int(input("PLs enter a second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+product = num1 * num2
+quotient = num1/ num2
+remainder = num1 % num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Product:", product)
+print("quotient:", quotient)
+print("remainder:", remainder)
+
+
